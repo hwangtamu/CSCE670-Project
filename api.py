5050d9dbe4a72e7f114a0c3549c021dbc289ab19
Han Wang <hanwang@tamu.edu>
2013-04-26 16:57:31 -0500

    getfollowing

--- api.py
+++ api.py
@@ -19,3 +19,19 @@ def searchTweets(q):
                 data[tweet['created_at'][:20]] = tweet['text']
             with open('./verified/' + str(q) + '.json', 'w') as f:
                 json.dump(data, f)
+
+def getfollower(q):
+    data = {}
+    data['ids'] = []
+    follower = urllib.urlopen('https://api.twitter.com/1/friends/ids.json?cursor=-1&user_id=' + str(q))
+    follow = simplejson.loads(follower.read())
+    for i in follow['ids']:
+        data['ids'].append(i)
+    while follow['next_cursor'] != 0:
+        follower = urllib.urlopen('https://api.twitter.com/1/friends/ids.json?cursor='
+                                  + follow['next_cursor_str'] + '&user_id=' + str(q))
+        follow = simplejson.loads(follower.read())
+        for i in follow['ids']:
+            data['ids'].append(i)
+    with open('./follower/' + str(q) + '.json', 'w') as f:
+        json.dump(data, f)
--- verified.py
+++ verified.py
@@ -1,8 +1,4 @@
 __author__ = 'han'
-import simplejson
-from api import searchTweets
+from api import getfollower
 
-with open('./1.json', 'r') as f:
-    d = simplejson.loads(f.read())
-for i in d['ids'][700:]:
-    searchTweets(i)
+getfollower(813286)
